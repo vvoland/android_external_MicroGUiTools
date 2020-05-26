440fe492716495a0f7b8aa60451e2379b53aa421
Woland <me@woland.xyz>
2020-05-26 13:43:49 +0200

    Don't generate BuildConfig

---
+++ microg-ui-tools/src/main/java/org/microg/tools/ui/BuildConfig.java
@@ -0,0 +1,6 @@
+package org.microg.tools.ui;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "org.microg.tools.ui";
+    public static final int VERSION_CODE = 1;
+}
